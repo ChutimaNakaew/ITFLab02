d25714fe2ca99b51df56f8b627507f9d61c6590b
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-11-20 19:16:46 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect('63070039itflab02.mysql.database.azure.com', 'chutima@63070039itflab02', 'Fah931755', 'ITFLab', 3306);
+	$conn = mysqli_connect('63070039itflab02.mysql.database.azure.com', 'chutima@63070039itflab02', 'Fah931755', 'ITFLab');
 
 	$name = $_POST['name'];
 	$comment = $_POST['comment'];
