ff6cb7c85ee72553e28c93f01fc9f74a05da3cd4
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-11-20 21:02:31 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -25,6 +25,12 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
   <title>Insert</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
+<style>
+body {
+  background-color: #79a2c7
+;
+}
+</style>
 
 <body>
   <div class="container mt-5 text-center">
