239c6362a87aae211905f4ec6925d4739941feec
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-11-20 21:06:56 +0700

    Update update.php

--- update.php
+++ update.php
@@ -19,6 +19,12 @@
     <title>Update</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
+<style>
+body {
+  background-color: #ffd1c7
+;
+}
+</style>
 
 <body>
     <div class="container mt-5 text-center">
