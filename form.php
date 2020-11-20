a49bb5ced47797f704a4c3721ebd5fe7b4b0ec23
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-11-20 20:35:14 +0700

    Update form.php

--- form.php
+++ form.php
@@ -8,8 +8,8 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <style>
-body { 
-  background: lightblue url("img_tree.gif") no-repeat fixed center; 
+body {
+  background-color: #ff9966;
 }
 </style>
 <body>
