46698e28031d64fd2a93cda4ccc575d46a4b9da9
ChutimaNakaew <73680207+ChutimaNakaew@users.noreply.github.com>
2020-11-20 18:16:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
   <title>DATABASE</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -22,23 +25,12 @@
        <a href="form.php" class="btn btn-warning">Add</a>
     </div>
     <div class="card-body">
-      <div class="table-responsive">
-        <table class="table table-hover table-bordered table-sm">
-          <thead class="thead-secondary">
-            <tr>
-              <th width="300">
-                <div align="center">Name</div>
-              </th>
-              <th width="300">
-                <div align="center">Comment </div>
-              </th>
-              <th width="300">
-                <div align="center">Link </div>
-              </th>
-              <th width="300">
-                <div align="center">Action</div>
-              </th>
-            </tr>
+      <div class="container">
+        <h2>Dark Striped Table</h2>
+        <p>Combine .table-dark and .table-striped to create a dark, striped table:
+      </p>            
+        <table class="table table-dark table-striped">
+    </table>
           </thead>
           <tbody>
             <?php
